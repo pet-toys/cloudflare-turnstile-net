6ff80331ef339adffcab957d008d5ebc91a59807
andbaz <dev@andbaz.slmail.me>
2025-09-10 13:12:56 +0300

    upgraded dependencies

--- test/PetToys.CloudflareTurnstileNet.Tests/ServiceCollectionExtensionsTest.cs
+++ test/PetToys.CloudflareTurnstileNet.Tests/ServiceCollectionExtensionsTest.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using FluentAssertions;
+using AwesomeAssertions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
--- test/PetToys.CloudflareTurnstileNet.Tests/TurnstileServiceTest.cs
+++ test/PetToys.CloudflareTurnstileNet.Tests/TurnstileServiceTest.cs
@@ -1,6 +1,6 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
